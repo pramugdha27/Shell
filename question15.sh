9f3caa8156cdeb4a16181e9c1c0da3daef690d78
Pramugdha Chowdhury <pramugdha.momo@gmail.com>
2023-06-02 00:19:57 +0930

    Question15

--- question15.sh
+++ question15.sh
@@ -1,3 +1,6 @@
-git fetch git@github.com:ian-knight-uofa/git-practice-01.git
+git fetch git@github.com:ian-knight-uofa/git-practice-01.git branch1
 git checkout main
+git rm file1 dir1/file3
+git add file1 dir1/file3
+git commit -m "q15"
 git merge branch1
