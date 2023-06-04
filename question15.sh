abc13666f1ebbcbc8a00bab85ba20bb25a6e8fbe
Pramugdha Chowdhury <pramugdha.momo@gmail.com>
2023-06-04 22:12:01 +0930

    Question15.sh

--- question15.sh
+++ question15.sh
@@ -1,6 +1,10 @@
-git fetch git@github.com:ian-knight-uofa/git-practice-01.git branch1
-git checkout main
-git rm file1 dir1/file3
-git add file1 dir1/file3
-git commit -m "q15"
+git clone git@github.com:ian-knight-uofa/git-practice-01.git
+cd  git-practice-01
 git merge branch1
+git checkout --theirs file1
+rm dir1/file3
+cd dir1
+touch file3
+echo "Other changes that are also important.
+These changes are important. They should be included in the final version alongside any other changes." >> file3
+
